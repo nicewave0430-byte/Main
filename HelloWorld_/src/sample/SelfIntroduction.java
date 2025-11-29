6993ca31e9f7c0b78c92ad3fd404608e971892df
nicewave0430-byte <nicewave0430@gmail.com>
2025-11-29 10:36:07 +0900

    Hinata Ono

---
+++ HelloWorld_/src/sample/SelfIntroduction.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class SelfIntroduction {
+
+    public static void main(String[] args) {
+
+        String name  = "小野 陽向";
+        int age      = 23;
+        String hobby = "音楽制作";
+
+        System.out.println("私の名前は " + name  + " です。");
+        System.out.println("年齢は "   + age   + " 歳です。");
+        System.out.println("趣味は "   + hobby + " です。");
+    }
+}
