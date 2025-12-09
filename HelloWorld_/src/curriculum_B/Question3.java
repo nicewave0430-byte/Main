530cdeba969b4e49ede55102090239916abf7101
nicewave0430-byte <nicewave0430@gmail.com>
2025-12-09 11:47:20 +0900

    Java課題3 インデント修正

--- HelloWorld_/src/curriculum_B/Question3.java
+++ HelloWorld_/src/curriculum_B/Question3.java
@@ -6,97 +6,90 @@ public class Question3 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 		//Q1
-	 for (int i = 1; i <= 10; i++) {
-       System.out.println(i);
-	}
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
 		//Q2
-      for (int i = 2; i <= 20; i += 2) {
-	  System.out.println(i);
-		        }
-      //Q3
-      for (int i = 10; i >= 1; i--) {
-          System.out.println(i);
-      }
-      //Q4
-      int sum = 0;  
+		for (int i = 2; i <= 20; i += 2) {
+			System.out.println(i);
+		}
+		//Q3
+		for (int i = 10; i >= 1; i--) {
+			System.out.println(i);
+		}
+		//Q4
+		int sum = 0;
 
-      for (int i = 1; i <= 100; i++) {
-          sum += i;   
-      }
+		for (int i = 1; i <= 100; i++) {
+			sum += i;
+		}
 
-      System.out.println("合計: " + sum);	
-	
-	//Q5
-	  for (int i = 1; i <= 5; i++) {    
-      for (int j = 1; j <= i; j++) { 
-      System.out.print("*");
-	   }
-	    System.out.println(); }
-	    
-	 //Q6
-	    int i2 = 1;            
+		System.out.println("合計: " + sum);
 
-	    while (i2 <= 10) {     
-	        System.out.println(i2);
-	        i2++;              
-	    }
-	 //Q7
-	    int i3 = 2;            
+		//Q5
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
-	    while (i3 <= 20) {     
-	        System.out.println(i3);
-	        i3 += 2;           
-	    }
-	    //Q8
-	    int i4 = 10;
+		//Q6
+		int i2 = 1;
 
-	    while (i4 >= 1) {
-	        System.out.println(i4);
-	        i4--;
-	    }
-	    //Q9
-	    int i5 = 1;            
-	    int sum1 = 0;          
+		while (i2 <= 10) {
+			System.out.println(i2);
+			i2++;
+		}
+		//Q7
+		int i3 = 2;
 
-	    while (i5 <= 100) {    
-	        sum1 += i5;         
-	        i5++;              
-	    }
-	    System.out.println(sum1);
-	    
-	    //Q10
-	    Scanner sc = new Scanner(System.in);  
-        int num = 0;                          
+		while (i3 <= 20) {
+			System.out.println(i3);
+			i3 += 2;
+		}
+		//Q8
+		int i4 = 10;
 
-        while (true) {                        
-        System.out.print("数値を入力してください：");
-        num = sc.nextInt();               
+		while (i4 >= 1) {
+			System.out.println(i4);
+			i4--;
+		}
+		//Q9
+		int i5 = 1;
+		int sum1 = 0;
 
-        if (num == 0) {                   
-            System.out.println("終了しました");
-            break;                        
-        }
-    }
+		while (i5 <= 100) {
+			sum1 += i5;
+			i5++;
+		}
+		System.out.println(sum1);
 
-        sc.close();
-    
-        //Q11
-        for (int i6 = 1; i6 <= 9; i6++) {         
-        for (int j = 1; j <= 9; j++) {       
-                        int result = i6 * j;
-                        System.out.printf("%02d x %02d = %02d | ", i6, j, result);
-                    }
-                    System.out.println();                
-                }
-            }
+		//Q10
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+
+		while (true) {
+			System.out.print("数値を入力してください：");
+			num = sc.nextInt();
+
+			if (num == 0) {
+				System.out.println("終了しました");
+				break;
+			}
+		}
+
+		sc.close();
+
+		//Q11
+		for (int i6 = 1; i6 <= 9; i6++) {
+			for (int j = 1; j <= 9; j++) {
+				int result = i6 * j;
+				System.out.printf("%02d x %02d = %02d | ", i6, j, result);
+			}
+			System.out.println();
+		}
 	}
-
-
-    
-
-
-	    
-
-
+}
